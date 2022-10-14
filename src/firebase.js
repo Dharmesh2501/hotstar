304e2ccf1476dee045730e57fe5d3d3ad5fd7809
RUSHIL <69619062+rushilmakvana@users.noreply.github.com>
2022-10-14 19:24:37 +0530

    initializing firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,36 +1,3 @@
-// import firebase from "firebase/app";
-
-// const firebaseConfig = {
-//     apiKey: "AIzaSyCXn_kX9X5dBtqrXVX3qadsSWjo6x1bx0I",
-//     authDomain: "hotstar-clone-fdd7b.firebaseapp.com",
-//     projectId: "hotstar-clone-fdd7b",
-//     storageBucket: "hotstar-clone-fdd7b.appspot.com",
-//     messagingSenderId: "795314593363",
-//     appId: "1:795314593363:web:74eb63dbf01be543bb30a6",
-//     measurementId: "G-BC8BK77FSQ"
-//   };
-
-// //   // Initialize Firebase
-//    const firebaseApp = firebase.initializeApp({
-//     apiKey: "AIzaSyCXn_kX9X5dBtqrXVX3qadsSWjo6x1bx0I",
-//     authDomain: "hotstar-clone-fdd7b.firebaseapp.com",
-//     projectId: "hotstar-clone-fdd7b",
-//     storageBucket: "hotstar-clone-fdd7b.appspot.com",
-//     messagingSenderId: "795314593363",
-//     appId: "1:795314593363:web:74eb63dbf01be543bb30a6",
-//     measurementId: "G-BC8BK77FSQ"
-//   });
-//    const db=firebaseApp.firestore();    
-//    const auth = firebase.auth();
-//    const provider =  firebase.auth.GoogleAuthProvider();
-//    const storage = firebase.storage();
-// //    const analytics = getAnalytics(app);
-
-// export { auth, provider, storage};
-// export default db;
-
-// import fire from 'firebase'
-
 import { initializeApp } from 'firebase/app';
 import {getFirestore,collection,getDoc} from 'firebase/firestore/lite';
 import {getAuth} from "firebase/auth";
@@ -41,12 +8,13 @@ import { GoogleAuthProvider } from "firebase/auth";
 // Use your config values here.
 const app=initializeApp({
     apiKey: "AIzaSyCXn_kX9X5dBtqrXVX3qadsSWjo6x1bx0I",
+    measurementId: "G-BC8BK77FSQ",
+     messagingSenderId: "795314593363",
         authDomain: "hotstar-clone-fdd7b.firebaseapp.com",
         projectId: "hotstar-clone-fdd7b",
         storageBucket: "hotstar-clone-fdd7b.appspot.com",
-        messagingSenderId: "795314593363",
         appId: "1:795314593363:web:74eb63dbf01be543bb30a6",
-        measurementId: "G-BC8BK77FSQ"
+        
 });
 
 
